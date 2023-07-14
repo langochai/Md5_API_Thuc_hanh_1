c1fc4e8c3e07d941e8e68a37b67b52c2fbd78d02
langochai <langochai11121998@gmail.com>
2023-07-14 10:02:12 +0700

    update post method

--- mock-api/users/POST.js
+++ mock-api/users/POST.js
@@ -1,5 +1,14 @@
 module.exports = (req, res) => {
-    return res.status(201).send({
+    const editJsonFile = require("edit-json-file");
+    let file = editJsonFile("./mock-api/users/GET.json")
+    file.set(req.body.id.toString(),{
+        id: req.body.id,
+        name: req.body.name,
+        birthday: req.body.birthday
+    })
+    file.data.splice(-2,1)
+    file.save()
+    return res.status(201).json({
         id: req.body.id,
         name: req.body.name,
         birthday: req.body.birthday
